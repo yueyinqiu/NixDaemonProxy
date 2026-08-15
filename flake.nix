5236516c29062a444ad889847231c9ff6f262b37
yueyinqiu <yueyinqiu@outlook.com>
2026-08-15 23:36:30 +0800

    client as well

--- flake.nix
+++ flake.nix
@@ -29,13 +29,16 @@
           default = pkgs.mkShell {
             packages = [
               pkgs.dotnetCorePackages.sdk_10_0
-              (pkgs.writeShellScriptBin "dev-publish-server" ''
+              (pkgs.writeShellScriptBin "dev-publish" ''
                 set -euo pipefail
                 mkdir -p publish
                 temp=$(mktemp -d -p publish)
 
                 dotnet publish src/NixDaemonProxy.Server/NixDaemonProxy.Server.csproj -c Release -o "$temp/NixDaemonProxy.Server"
                 ouch compress "$temp/NixDaemonProxy.Server"/* "$temp/NixDaemonProxy.Server.zip"
+
+                dotnet publish src/NixDaemonProxy.Client/NixDaemonProxy.Client.csproj -c Release -o "$temp/NixDaemonProxy.Client"
+                ouch compress "$temp/NixDaemonProxy.Client"/* "$temp/NixDaemonProxy.Client.zip"
               '')
             ];
             shellHook = ''
